e20fec909d559b75a3c381c8b59202fd3003c4bf
Squid90 <squid90@rambler.ru>
2024-05-31 00:41:21 +0300

    U8

--- src/js/TicketService.js
+++ src/js/TicketService.js
@@ -1,6 +1,7 @@
 const xhr = new XMLHttpRequest();
 export default class TicketService {
-  list(callback) {
+  // eslint-disable-next-line
+  list() {
     xhr.open('GET', 'http://localhost:7070/?method=allTickets');
 
     xhr.send();
@@ -26,24 +27,27 @@ export default class TicketService {
           data.forEach((element) => {
             target.innerHTML += `
 
-                  <div class="container" style=" width: 100%; height: 50px; border: 1px solid black; display: flex; align-items: center; justify-content: space-between;">
+                  <div class="box" style=" width: 100%; min-height: 50px; border: 1px solid black; display: flex; align-items: center; justify-content: space-between; flex-wrap: wrap;">
                     <div class="container1" style="width: 5%;display: flex; align-items: center;">
                         <input type="checkbox" class="circle-checkbox" style="width: 16px; height: 16px; border-radius: 50%; border: 1px solid black; outline: none;">
                     </div>
-                    <div class="container2" style="width: 40%;display: flex; align-items: center;">
-                      <div>${element.name}</div>
+                    <div class="container container2" style="width: 40%; height: 50px; display: flex; align-items: center;">
+                      ${element.name}
                     </div>
-                    <div class="containerX" style="display: flex; width: 20%;">
-                      
+                    <div class="container containerX" style="display: flex; width: 20%; height: 50px; ">
+                      <div class="content" style="display: none;">${element.id}</div>
                     </div>
-                    <div class="container3" style="width: 25%;text-align: right;">
-                        <div>${formatDateAndTime(element.created)}</div>
+                    <div class="container container3" style="width: 25%; height: 50px; display: flex; align-items: center; justify-content: right;">
+                        ${formatDateAndTime(element.created)}
                     </div>
                     <div class="container4" style="width: 5%; text-align: right;">
                         <button class="round-button" style="display: inline-block; width: 30px; height: 30px; border-radius: 50%; border: 1px solid black; text-align: center;">E</button>
                     </div>
                     <div class="container5" style="width: 5%; text-align: right;">
                         <button class="round-button" style="display: inline-block; width: 30px; height: 30px; border-radius: 50%; border: 1px solid black; text-align: center;">X</button>
+                    </div>
+                    <div class="description"  style="display: none; width: 100%; margin-top: 5px; margin-bottom: 5px; border: 1px solid black; min-height: 30px;">
+                    ${element.description}
                     </div>
                   </div>
                   `;
@@ -54,12 +58,4 @@ export default class TicketService {
       }
     });
   }
-
-  get(id, callback) {}
-
-  create(data, callback) {}
-
-  update(id, data, callback) {}
-
-  delete(id, callback) {}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,130 @@ app.init();
 
 const addForm = document.querySelector('.addForm');
 const newTicket = document.querySelector('.add-button');
+const okNewTicket = document.querySelector('.ok-button');
 newTicket.addEventListener('click', () => {
-  console.log('click');
   if (addForm.style.display === 'none') {
     addForm.style.display = 'flex';
+
+    okNewTicket.addEventListener('click', () => {
+      // eslint-disable-next-line
+      event.preventDefault();
+      const xhr = new XMLHttpRequest();
+      const createForm = document.querySelector('.addForm');
+      const inputName = createForm.querySelector('.name');
+      const inputdescription = createForm.querySelector('.description');
+      const inputData = {
+        name: inputName.value,
+        description: inputdescription.value,
+      };
+      const data = JSON.stringify(inputData);
+
+      xhr.open('POST', 'http://localhost:7070?method=createTicket');
+      xhr.send(data);
+      inputName.value = '';
+      inputdescription.value = '';
+      addForm.style.display = 'none';
+    });
   } else {
     addForm.style.display = 'none';
   }
 });
+const cancelNewTicket = document.querySelector('.cancel-button');
+cancelNewTicket.addEventListener('click', () => {
+  // eslint-disable-next-line
+  event.preventDefault();
+  const createForm = document.querySelector('.addForm');
+  const inputName = createForm.querySelector('.name');
+  const inputdescription = createForm.querySelector('.description');
+  inputName.value = '';
+  inputdescription.value = '';
+  addForm.style.display = 'none';
+});
+
+setTimeout(() => {
+  const rootCard = document.querySelector('.root');
+  const shortCard = rootCard.querySelectorAll('.box');
+  shortCard.forEach((item) => {
+    const container = item.querySelectorAll('.container');
+    container.forEach((element) => {
+      element.addEventListener('click', () => {
+        const longCard = item.querySelector('.description');
+        if (longCard.style.display === 'none') {
+          longCard.style.display = 'flex';
+        } else {
+          longCard.style.display = 'none';
+        }
+      });
+    });
+  });
+}, 1000);
+
+setTimeout(() => {
+  const xhr = new XMLHttpRequest();
+  const rootCard = document.querySelector('.root');
+  const shortCard = rootCard.querySelectorAll('.box');
+  shortCard.forEach((item) => {
+    const delButton = item.querySelectorAll('.container5');
+    const boxId = item.querySelector('.content').textContent;
+    delButton.forEach((element) => {
+      element.addEventListener('click', () => {
+        xhr.open('GET', `http://localhost:7070/?method=deleteById&id=${boxId}`);
+        xhr.send();
+      });
+    });
+  });
+}, 1000);
+
+setTimeout(() => {
+  const editForm = document.querySelector('.editForm');
+  const okEditTicket = document.querySelector('.ok-edit-button');
+  const rootCard = document.querySelector('.root');
+  const shortCard = rootCard.querySelectorAll('.box');
+  shortCard.forEach((item) => {
+    const editButton = item.querySelectorAll('.container4');
+    const shortName = item.querySelector('.container2');
+    const longDescription = item.querySelector('.description');
+    editButton.forEach((element) => {
+      element.addEventListener('click', () => {
+        if (editForm.style.display === 'none') {
+          editForm.style.display = 'flex';
+          const editInputName = editForm.querySelector('.edit-name');
+          const editInputdescription = editForm.querySelector('.edit-description');
+          const boxId = item.querySelector('.content').textContent;
+          editInputName.value = shortName.textContent;
+          editInputdescription.value = longDescription.textContent;
+
+          okEditTicket.addEventListener('click', () => {
+            // eslint-disable-next-line
+            event.preventDefault();
+            const xhr = new XMLHttpRequest();
+
+            const inputEditData = {
+              name: editInputName.value,
+              description: editInputdescription.value,
+            };
+            const editData = JSON.stringify(inputEditData);
+
+            xhr.open('POST', `http://localhost:7070?method=updateById&id=${boxId}`);
+            xhr.send(editData);
+            editInputName.value = '';
+            editInputdescription.value = '';
+            editForm.style.display = 'none';
+          });
+        } else {
+          editForm.style.display = 'none';
+        }
+        const cancelEditTicket = document.querySelector('.cancel-edit-button');
+        cancelEditTicket.addEventListener('click', () => {
+          // eslint-disable-next-line
+          event.preventDefault();
+          const editInputName = editForm.querySelector('.edit-name');
+          const editInputdescription = editForm.querySelector('.edit-description');
+          editInputName.value = '';
+          editInputdescription.value = '';
+          editForm.style.display = 'none';
+        });
+      });
+    });
+  });
+}, 1000);
